26c4387b1db0eefd7cb971d5afb57240851ffba2
ValeriaL <83677619+Valeria-Lopez@users.noreply.github.com>
2024-04-20 02:34:54 -0600

    Update GDorogovtsevMendes.py

--- Graph/Modelos/GDorogovtsevMendes.py
+++ Graph/Modelos/GDorogovtsevMendes.py
@@ -9,18 +9,23 @@ from Clases import Grafo, Nodo, Edge, Undirected_graph
 import random
 
 
-def graphDorogov_Mendes(n):
+def graphDorogov_Mendes(n, nodo_s=0):
     """Genera un grafo con el modelo de Dorogovtsev-Mendes.
     Crea 3 nodos y 3 aristas formando un triángulo, cada nodo adicional
     se une con los nodos que forman la arista selecionada al azar.
+    También se generan los grafos de BFS y DFS (recursivo e iterativo).
     
     Args:
         n (int): Indica la cantidad de nodos en el grafo.
+        nodo_s (int): Nodo raíz desde donde se inician las búsquedas.
         
     Returns:
         _DOT_: Archivo que contiene los nodos y aristas del grafo.
     """
     g = Grafo()
+    
+    if nodo_s > n+3:
+        raise ValueError()
     
     nodos_list = []
     edge_dict = []
@@ -46,4 +51,7 @@ def graphDorogov_Mendes(n):
             edge_dict.append([nodos,k])
             
     g.save_gephi('dorogovtsevMendes' + str(n))
-    return print('ok')
+    g.bfs(g.get_nodo(nodo_s), 'dorogovtsevMendes_bfs' + str(n))
+    g.dfs_recursivo(g.get_nodo(nodo_s), 'dorogovtsevMendes_dfsrec' + str(n))
+    g.dfs_iterativo(g.get_nodo(nodo_s), 'dorogovtsevMendes_dfsit' + str(n))
+
