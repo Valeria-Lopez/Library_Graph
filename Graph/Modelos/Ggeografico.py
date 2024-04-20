40e5e67fdd33a98f61908db5032ad7804ad7ab74
ValeriaL <83677619+Valeria-Lopez@users.noreply.github.com>
2024-04-20 02:40:23 -0600

    Update Ggeografico.py

--- Graph/Modelos/Ggeografico.py
+++ Graph/Modelos/Ggeografico.py
@@ -11,13 +11,14 @@ import random
 import numpy as np
 
 
-def geoGraph(n, r):
+def geoGraph(n, r, nodo_s=0):
     """Genera un grafo con el modelo geográfico simple.
     Crea n nodos y crea una arista entre otros nodos que esten a una distancia
-    r o menor.
+    r o menor. También se generan los grafos de BFS y DFS (recursivo e iterativo).
     Args:
         n (int): Indica la cantidad de nodos en el grafo.
         r (float): Distancia máxima para crear una arista.
+        nodo_s (int): Nodo raíz desde donde se inician las búsquedas.
         
     Returns:
         _DOT_: Archivo que contiene los nodos y aristas del grafo.
@@ -25,6 +26,9 @@ def geoGraph(n, r):
     g = Grafo()
     
     if n < 1 or r < 0:
+        raise ValueError()
+        
+    if nodo_s > n:
         raise ValueError()
         
     nodos_list = []
@@ -47,5 +51,7 @@ def geoGraph(n, r):
             if distancia <= r:
                 g.add_edge(Edge(g.get_nodo(nodos_list[i]), g.get_nodo(nodos_list[j])))
            
-    g.save_gephi('geografico' + str(n) + '_' + str(r))            
-    return print('ok')
+    g.save_gephi('geografico' + str(n) + '_' + str(r))
+    g.bfs(g.get_nodo(nodo_s), 'geografico_bfs' + str(n) + '_' + str(r))
+    g.dfs_recursivo(g.get_nodo(nodo_s), 'geografico_dfsrec' + str(n) + '_' + str(r))
+    g.dfs_iterativo(g.get_nodo(nodo_s), 'geografico_dfsit' + str(n) + '_' + str(r))         
