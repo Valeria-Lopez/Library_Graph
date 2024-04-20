6d8c3276e4df6fdf670b74b50925828db4ff06e3
ValeriaL <83677619+Valeria-Lopez@users.noreply.github.com>
2024-04-20 02:38:30 -0600

    Update GbarabasiAlbert.py

--- Graph/Modelos/GbarabasiAlbert.py
+++ Graph/Modelos/GbarabasiAlbert.py
@@ -8,19 +8,24 @@ Created on Wed Apr  3 14:50:20 2024
 from Clases import Grafo, Nodo, Edge, Undirected_graph
 
 
-def barabasiAlbertGraph(n, d):
+def barabasiAlbertGraph(n, d, nodo_s=0):
     """Genera un grafo con el modelo de Barabási-Albert.
     Se coloca nodo por nodo y se asigna una cantidad de d aristas para que
     cada nuevo nodo se enlace.
+    También se generan los grafos de BFS y DFS (recursivo e iterativo).
     
     Args:
         n (int): Indica la cantidad de nodos en el grafo.
         d (int): Grado máximo esperado para cada uno de los nodos nuevos.
+        nodo_s (int): Nodo raíz desde donde se inician las búsquedas.
         
     Returns:
         _DOT_: Archivo que contiene los nodos y aristas del grafo.
     """
     g = Grafo()
+    
+    if nodo_s > n:
+        raise ValueError()
      
     nodos_list = []
     degree = {}
@@ -47,6 +52,9 @@ def barabasiAlbertGraph(n, d):
                     if degree[nodos] == d:
                         break
                     continue
+                
     g.save_gephi('barabasiAlbert' + str(n) + '_' + str(d))
-    return print('ok')
+    g.bfs(g.get_nodo(nodo_s), 'barabasiAlbert_bfs' + str(n) + '_' + str(d))
+    g.dfs_recursivo(g.get_nodo(nodo_s), 'barabasiAlbert_dfsrec' + str(n) + '_' + str(d))
+    g.dfs_iterativo(g.get_nodo(nodo_s), 'barabasiAlbert_dfsit' + str(n) + '_' + str(d))
          
